43a82a94ba4a87346365a1ecf09c79e875e48ff3
Akshay Dixit <akshaydixi@gmail.com>
2013-04-26 14:50:28 +0530

    fixed adj bug

--- graph.py
+++ graph.py
@@ -1,6 +1,6 @@
 class Graph:
 	def __init__(self,V):
-		self.adj={}
+		self.adjacent={}
 		self.E=0
 
 		if str(type(V))[7:-2] == 'int':
@@ -15,7 +15,7 @@ class Graph:
 			x = eval(lines[0].strip())
 			self.V = x
 			for v in range(1,x+1):
-				self.adj[v]=[]
+				self.adjacent[v]=[]
 			for line in lines[1:]:
 				self.E+=1
 				line = line.strip().split()
@@ -24,16 +24,16 @@ class Graph:
 				self.addEdge(v,w)
 
 	def addEdge(self,v,w):
-		self.adj[v].append(w);
-		self.adj[w].append(v);
+		self.adjacent[v].append(w);
+		self.adjacent[w].append(v);
 		self.E+=1
 
 	def adj(self,v):
-		return self.adj[v];
+		return self.adjacent[v]
 
 	def show(self):
 		for v in range(1,self.V+1):
-			print v,':',self.adj[v]
+			print v,':',self.adj(v)
 
 	def vertices(self):
 		return self.V
